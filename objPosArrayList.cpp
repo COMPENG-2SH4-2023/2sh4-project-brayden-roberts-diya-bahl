082b371bcbc4f4c09e7019bba230be86d41728a3
Diya <bahld1@mcmaster.ca>
2023-12-03 22:59:54 -0500

    commented objPosArrayList.cpp file

--- objPosArrayList.cpp
+++ objPosArrayList.cpp
@@ -1,11 +1,13 @@
 #include "objPosArrayList.h"
 
+// Constructor for objPosArrayList class
 objPosArrayList::objPosArrayList()
 {
     sizeArray = 0;
-    aList = new objPos[ARRAY_MAX_CAP];
+    aList = new objPos[ARRAY_MAX_CAP]; // Allocate dynamic memory for the array with a maximum capacity
 }
 
+// Destructor for objPosArrayList class
 objPosArrayList::~objPosArrayList()
 {
     for(int i = 0; i < sizeArray; i++)
@@ -14,11 +16,13 @@ objPosArrayList::~objPosArrayList()
     }
 }
 
+// Get the current size of the array
 int objPosArrayList::getSize()
 {
     return sizeArray;
 }
 
+// Insert a new objPos at the head of the array
 void objPosArrayList::insertHead(objPos thisPos)
 {
     int i;
@@ -33,12 +37,14 @@ void objPosArrayList::insertHead(objPos thisPos)
     sizeArray++;
 }
 
+// Insert a new objPos at the tail of the array
 void objPosArrayList::insertTail(objPos thisPos)
 {
     thisPos.getObjPos(aList[sizeArray]);
     sizeArray++;
 } 
 
+// Remove the element at the head of the array
 void objPosArrayList::removeHead()
 {
     int i;
@@ -48,26 +54,30 @@ void objPosArrayList::removeHead()
         aList[i + 1].getObjPos(aList[i]);
     }
 
-    removeTail();
+    removeTail(); // Remove the tail to adjust the size
     
 }
 
+// Remove the element at the tail of the array
 void objPosArrayList::removeTail()
 {
-    aList[sizeArray-1].setObjPos(0,0,0);
+    aList[sizeArray-1].setObjPos(0,0,0); // Set the tail element to a default objPos
     sizeArray--;
 }
 
+// Retrieve the objPos at the head of the array
 void objPosArrayList::getHeadElement(objPos &returnPos)
 {
     aList[0].getObjPos(returnPos);
 }
 
+// Retrieve the objPos at the tail of the array
 void objPosArrayList::getTailElement(objPos &returnPos)
 {
     aList[sizeArray-1].getObjPos(returnPos);
 }
 
+// Retrieve the objPos at a specific index in the array
 void objPosArrayList::getElement(objPos &returnPos, int index)
 {
     aList[index].getObjPos(returnPos);
